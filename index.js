bef521a2e0dbf43752a242f4822d55f174bbfeb5
Niko Heiskanen <nikoheiskanen12@kamk.fi>
2024-08-02 15:04:23 +0300

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+const cors = require('cors')
+
+app.use(cors())
 
 const morgan = require('morgan')
 
@@ -142,7 +145,7 @@ app.post('/api/persons', (request, response) => {
   app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
