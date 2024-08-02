f3669ef9732428a577ca599ea85a11422aa6840c
Niko Heiskanen <nikoheiskanen12@kamk.fi>
2024-08-02 15:28:45 +0300

    change to dist url

--- index.js
+++ index.js
@@ -56,13 +56,6 @@ let persons = [
     ]
 
 
-    const requestLogger = (request, response, next) => {
-      console.log('Method:', request.method)
-      console.log('Path:  ', request.path)
-      console.log('Body:  ', request.body)
-      console.log('---')
-      next()
-    }
 
 
 
